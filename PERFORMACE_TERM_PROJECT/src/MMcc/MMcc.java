78fad7aff29c88812e8228871e922e5460803056
CSu <likisee@gmail.com>
2015-06-14 00:47:52 +0800

    revise MMcc

--- PERFORMACE_TERM_PROJECT/src/MMcc/MMcc.java
+++ PERFORMACE_TERM_PROJECT/src/MMcc/MMcc.java
@@ -19,6 +19,11 @@ public class MMcc {
 		double nArrival = 0;
 		double nBlocking = 0;
 		
+		int nRound = 1000000;
+		double totalExecuteTime = 0.0;
+		double totalBlockRate = 0.0;
+		double totalElaspsedSeconds = 0.0;
+		
 		// Setting
 //		GammaDistribution M1 = new GammaDistribution(1, 1.0 / 50.0); // Arrival Rate
 //		GammaDistribution M2 = new GammaDistribution(1, 1.0 / 10.0); // Service Rate
@@ -32,50 +37,60 @@ public class MMcc {
 		Event firstEvent = new Event(1, 0.0);
 		queue.add(firstEvent);
 		
-		// Start
-		long timeStart = System.currentTimeMillis();
-		double maxTs = 0;
-		while(nArrival <= bound) {
-			
-			Event event = queue.remove();
-			et = event.eventType;
-			ts = event.timeStamp;
-			
-			switch(et) {
-			case ARRIVAL:
+		for(int i = 0; i < nRound; i++) {
+			// Start
+			long timeStart = System.currentTimeMillis();
+			double maxTs = 0;
+			while(nArrival <= bound) {
 				
-				// Generating Next Event
-				nArrival++;
-				event = new Event(1, ts + M1.sample());
-				queue.add(event);
+				Event event = queue.remove();
+				et = event.eventType;
+				ts = event.timeStamp;
 				
-				if(c1 > 0) {
-					// Do Service
-					c1--;
-					event = new Event(2, ts + M2.sample());
-					queue.add(event);	
-				} else {
-					// Skip
-					nBlocking++;
+				switch(et) {
+				case ARRIVAL:
+					
+					// Generating Next Event
+					nArrival++;
+					event = new Event(1, ts + M1.sample());
+					queue.add(event);
+					
+					if(c1 > 0) {
+						// Do Service
+						c1--;
+						event = new Event(2, ts + M2.sample());
+						queue.add(event);	
+					} else {
+						// Skip
+						nBlocking++;
+					}
+					break;
+				case DEPARTURE:
+					c1++;
+					
+					if(ts > maxTs) maxTs = ts;
+					
+					break;
+					
 				}
-				break;
-			case DEPARTURE:
-				c1++;
-				
-				if(ts > maxTs) maxTs = ts;
-				
-				break;
 				
 			}
+			long timeEnd = System.currentTimeMillis();
 			
+			// A Round
+			System.out.println("A Execute Time: " + (timeEnd - timeStart) + " milliseconds");
+			System.out.println("A Blocking Rate: " + nBlocking / nArrival);
+			System.out.println("A Elaspsed seconds: " + maxTs);
+			
+			totalExecuteTime += timeEnd - timeStart;
+			totalBlockRate += nBlocking / nArrival;
+			totalElaspsedSeconds += maxTs;
 		}
-		long timeEnd = System.currentTimeMillis();
 		
-		// FINAL
-		System.out.println("Execute Time: " + (timeEnd - timeStart) + " milliseconds");
-		System.out.println("Blocking Rate = " + nBlocking / nArrival);
-		System.out.println("Elaspsed seconds(s) = " + maxTs);
-		
+		// Final
+		System.out.println("Avg Execute Time: " + totalExecuteTime/1000 + " seconds");
+		System.out.println("Avg Blocking Rate: " + totalBlockRate / nRound);
+		System.out.println("Avg Elaspsed seconds: " + totalElaspsedSeconds / nRound);
 	}
 
 }
